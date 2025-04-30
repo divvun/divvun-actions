9121d9fe815ba4747505c1670e28ce110da9c214
Brendan Molloy <brendan@necessary.nu>
2025-04-30 14:08:32 +0200

    oh

--- builder/buildkite/mod.ts
+++ builder/buildkite/mod.ts
@@ -2,7 +2,7 @@
 // Buildkite implementation of the builder interface
 
 import type { ExecOptions } from "~/builder/types.ts"
-import { buildkite as getEnv, Env } from "~/util/env.ts"
+import { Env, buildkite as getEnv } from "~/util/env.ts"
 import logger from "~/util/log.ts"
 import { OpenBao, SecretsStore } from "~/util/openbao.ts"
 
@@ -159,5 +159,6 @@ export async function group(name: string, callback: () => Promise<void>) {
   } catch (error) {
     console.log(`^^^ +++`)
     logger.error(error)
+    throw error
   }
 }
