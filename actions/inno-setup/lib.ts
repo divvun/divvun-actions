f6020e084cd00d438a2a37248fc0c0689e440525
Brendan Molloy <brendan@necessary.nu>
2025-05-05 20:18:54 +0200

    uhg

--- actions/inno-setup/lib.ts
+++ actions/inno-setup/lib.ts
@@ -12,8 +12,11 @@ export async function makeInstaller(
   //   `http://192.168.122.1:5000 ` +
   //   `-o $f"`
 
-  const proc = new Deno.Command(`iscc.exe /S"signtool=divvun-actions sign $f"`, {
+  const proc = new Deno.Command("pwsh", {
     args: [
+      "-Command",
+      `iscc.exe`,
+      `/S"signtool=divvun-actions sign $f"`,
       "/Qp",
       `/O${installerOutput}`,
       ...defines,
