482d74b1dccdeb367e53364e873d52ab724e27fb
Brendan Molloy <brendan@necessary.nu>
2025-05-05 20:33:40 +0200

    wat

--- actions/inno-setup/lib.ts
+++ actions/inno-setup/lib.ts
@@ -13,12 +13,12 @@ export async function makeInstaller(
   //   `http://192.168.122.1:5000 ` +
   //   `-o $f"`
 
-  const scriptPath = `${target.projectPath}\\bin\\divvun-actions.ps1`
+  const scriptPath = `"${target.projectPath}\\bin\\divvun-actions.ps1"`
   console.log(scriptPath)
 
   const proc = new Deno.Command("iscc.exe", {
     args: [
-      `/S"signtool=${scriptPath} sign $f"`,
+      `/S"signtool=pwsh -Command ${scriptPath} sign $f"`,
       "/Qp",
       `/O${installerOutput}`,
       ...defines,
