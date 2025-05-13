2dae2a2dd942ae6dd008ff2f8620346fd0e93f71
Brendan Molloy <brendan@necessary.nu>
2025-05-13 14:34:42 +0200

    wtf

--- util/openbao.ts
+++ util/openbao.ts
@@ -29,24 +29,26 @@ export class OpenBao {
     })
 
     const roleResponse =
-      (await client["/auth/{approle_mount_path}/role/{role_name}/role-id"].get({
+      await client["/auth/{approle_mount_path}/role/{role_name}/role-id"].get({
         params: {
           approle_mount_path: "approle",
           role_name: "builder",
         },
-      }).json())?.data
+      }).json()
     const secretResponse =
-      await (client["/auth/{approle_mount_path}/role/{role_name}/secret-id"]
+      await client["/auth/{approle_mount_path}/role/{role_name}/secret-id"]
         .post({
           params: {
             approle_mount_path: "approle",
             role_name: "builder",
           },
           json: {},
-        }).json())?.data
+        }).json()
 
-    const { role_id: roleId } = roleResponse
-    const { secret_id: roleSecret } = secretResponse
+    console.log(roleResponse, roleResponse.data)
+
+    const { role_id: roleId } = roleResponse?.data
+    const { secret_id: roleSecret } = secretResponse?.data
 
     if (endpoint == null || roleId == null || roleSecret == null) {
       throw new Error("OpenBao endpoint, roleId or roleSecret not found")
