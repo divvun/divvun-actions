abd70320159349c371a2bd2ed058886f880358b6
Brendan Molloy <brendan@necessary.nu>
2025-05-13 13:14:26 +0200

    weird

--- util/sslcom-codesigner.ts
+++ util/sslcom-codesigner.ts
@@ -1,23 +1,30 @@
-export async function sslComCodeSign(inputFile: string, secrets: {
-  username: string
-  password: string
-  credentialId: string
-  totpSecret: string
-}) {
-  const proc = new Deno.Command("CodeSignTool.bat", {
+export async function sslComCodeSign(
+  inputFile: string,
+  secrets: {
+    username: string;
+    password: string;
+    credentialId: string;
+    totpSecret: string;
+  }
+) {
+  const proc = new Deno.Command("cmd", {
     args: [
-      "sign",
-      `-input_file_path=${inputFile}`,
-      `-totp_secret=${secrets.totpSecret}`,
-      `-username=${secrets.username}`,
-      `-password=${secrets.password}`,
-      `-credential_id=${secrets.credentialId}`,
-      "-override",
+      "/c",
+      [
+        "CodeSignTool.bat",
+        "sign",
+        `-input_file_path=${inputFile}`,
+        `-totp_secret=${secrets.totpSecret}`,
+        `-username=${secrets.username}`,
+        `-password=${secrets.password}`,
+        `-credential_id=${secrets.credentialId}`,
+        "-override",
+      ].join(""),
     ],
-  }).spawn()
+  }).spawn();
 
-  const result = await proc.status
+  const result = await proc.status;
   if (result.code != 0) {
-    throw new Error(`Docker exited with code ${result.code}`)
+    throw new Error(` exited with code ${result.code}`);
   }
 }
