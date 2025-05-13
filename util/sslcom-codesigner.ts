f868fe77d9599e8297f6c0909f138c5e781c5f78
Brendan Molloy <brendan@necessary.nu>
2025-05-13 13:22:17 +0200

    wow

--- util/sslcom-codesigner.ts
+++ util/sslcom-codesigner.ts
@@ -1,30 +1,25 @@
-export async function sslComCodeSign(
-  inputFile: string,
-  secrets: {
-    username: string;
-    password: string;
-    credentialId: string;
-    totpSecret: string;
-  }
-) {
-  const proc = new Deno.Command("cmd", {
+export async function sslComCodeSign(inputFile: string, secrets: {
+  username: string
+  password: string
+  credentialId: string
+  totpSecret: string
+}) {
+  const proc = new Deno.Command("CodeSignTool.bat", {
     args: [
-      "/c",
-      [
-        "CodeSignTool.bat",
-        "sign",
-        `-input_file_path=${inputFile}`,
-        `-totp_secret=${secrets.totpSecret}`,
-        `-username=${secrets.username}`,
-        `-password=${secrets.password}`,
-        `-credential_id=${secrets.credentialId}`,
-        "-override",
-      ].join(" "),
+      "sign",
+      `-input_file_path=${inputFile}`,
+      `-totp_secret=${secrets.totpSecret}`,
+      `-username=${secrets.username}`,
+      `-password=${secrets.password}`,
+      `-credential_id=${secrets.credentialId}`,
+      "-override",
     ],
-  }).spawn();
+    // Unbelievably shit code.
+    cwd: "C:\\CodeSignTool",
+  }).spawn()
 
-  const result = await proc.status;
+  const result = await proc.status
   if (result.code != 0) {
-    throw new Error(`CodeSignTool exited with code ${result.code}`);
+    throw new Error(`CodeSignTool exited with code ${result.code}`)
   }
 }
