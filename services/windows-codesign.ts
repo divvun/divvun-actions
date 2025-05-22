38beaac62f07b2c0e5bfa10375bdb4d5cb86b156
Brendan Molloy <brendan@necessary.nu>
2025-05-22 16:42:57 +0200

    um

--- services/windows-codesign.ts
+++ services/windows-codesign.ts
@@ -1,25 +1,8 @@
-import * as path from "@std/path"
 import * as builder from "~/builder.ts"
 import { sslComCodeSign } from "../util/sslcom-codesigner.ts"
 
 export default async function sign(inputFile: string) {
   const secrets = await builder.secrets()
-  let signedPath = inputFile
-
-  const dir = path.dirname(inputFile)
-  const files = await Deno.readDir(dir)
-  console.log('##---##')
-  for await (const file of files) {
-    console.log(file.name)
-  }
-  console.log('##---##')
-
-  const hasInvalidExt = !inputFile.endsWith(".exe") && !inputFile.endsWith(".dll")
-  
-  if (hasInvalidExt) {
-    signedPath = `${inputFile}.exe`
-    await Deno.rename(inputFile, signedPath)
-  }
 
   await sslComCodeSign(inputFile, {
     username: secrets.get("sslcom/username"),
@@ -27,8 +10,4 @@ export default async function sign(inputFile: string) {
     credentialId: secrets.get("sslcom/credentialId"),
     totpSecret: secrets.get("sslcom/totpSecret"),
   })
-
-  if (hasInvalidExt) {
-    await Deno.rename(signedPath, inputFile)
-  }
 }
