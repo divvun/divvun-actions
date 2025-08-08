202924f5d17b429d5ce9dc7c6d3a83a0285b5d47
Brendan Molloy <brendan@necessary.nu>
2025-08-08 20:10:47 +0200

    Wow

--- actions/inno-setup/build.cmd
+++ actions/inno-setup/build.cmd
@@ -1,12 +1,17 @@
 @echo off
 
+REM Print all command line arguments for debugging
+echo All arguments: %*
+echo Arg 1 (scriptPath): %1
+echo Arg 2 (installerOutput): %2
+echo Arg 3 (issPath): %3
+
 REM Get command line arguments
-set "scriptPath=%1"
-set "installerOutput=%2"
-set "issPath=%3"
+set scriptPath=%1
+set installerOutput=%2
+set issPath=%3
 
 REM Build the iscc command arguments
-set "signToolArg="/Ssigntool=$q%scriptPath%$q sign $f""
+set signToolArg="/Ssigntool=%scriptPath% sign $f"
 
-REM Execute iscc.exe with all arguments
 iscc.exe %signToolArg% /Qp "/O%installerOutput%" %issPath%
