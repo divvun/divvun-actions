2057868248d2b88b32511ce10c5ea2a3cc6656eb
Brendan Molloy <brendan@necessary.nu>
2025-08-08 20:45:35 +0200

    remove

--- actions/inno-setup/build.cmd
+++ actions/inno-setup/build.cmd
@@ -1,15 +1,13 @@
 @echo off
 
-REM Print all command line arguments for debugging
-echo All arguments: %*
-echo Arg 1 (scriptPath): %1
-echo Arg 2 (installerOutput): %2
-echo Arg 3 (issPath): %3
+@REM echo All arguments: %*
+@REM echo Arg 1 (scriptPath): %1
+@REM echo Arg 2 (installerOutput): %2
+@REM echo Arg 3 (issPath): %3
 
-REM Get command line arguments
 set scriptPath=%1
 set installerOutput=%2
 set issPath=%3
 
-echo iscc.exe /S"signtool=$q%scriptPath%$q sign $f" /Qp "/O%installerOutput%" %issPath%
+@REM echo iscc.exe /S"signtool=$q%scriptPath%$q sign $f" /Qp "/O%installerOutput%" %issPath%
 iscc.exe /S"signtool=$q%scriptPath%$q sign $f" /Qp "/O%installerOutput%" %issPath%
