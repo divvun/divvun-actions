e3f0aab42dc867236fabff86a2c60fc1e671bec5
Dylan Hand <dylan.hand@gmail.com>
2025-08-14 16:44:14 +0200

    Try logging kbdgen output

--- util/shared.ts
+++ util/shared.ts
@@ -929,7 +929,7 @@ export class Kbdgen {
     logger.debug(`ANDROID_HOME: ${Deno.env.get("ANDROID_HOME")}`)
     logger.debug(`ANDROID_HOME: ${Deno.env.get("ANDROID_NDK_HOME")}`)
 
-    await Bash.runScript(
+    const output = await Bash.runScript(
       `kbdgen target --output-path output --bundle-path ${abs} android build`,
       {
         cwd,
@@ -947,6 +947,11 @@ export class Kbdgen {
         },
       },
     )
+
+    logger.debug("Output from kbdgen build: " + output[0])
+    for (const i in output) {
+      logger.debug(`Output line ${i}: ${output[i]}`)
+    }
 
     return await Kbdgen.resolveOutput(
       path.join(
