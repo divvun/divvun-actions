14984da76244f715b42b32c3f75516e89a5bab01
Brendan Molloy <brendan@necessary.nu>
2025-08-22 15:25:59 +0200

    Use real ISO lang list

--- actions/speller/manifest.ts
+++ actions/speller/manifest.ts
@@ -33,151 +33,190 @@ export function deriveLangTag() {
   // Map ISO 639-3 codes to ISO 639-1 codes where they exist
   // This is the complete set of ISO 639-1 languages that might be relevant
   const iso639_3_to_639_1: { [key: string]: string } = {
-    // Germanic languages
-    "afr": "af", // Afrikaans
-    "deu": "de", // German
-    "eng": "en", // English
-    "nld": "nl", // Dutch
-    "dan": "da", // Danish
-    "isl": "is", // Icelandic
-    "nor": "no", // Norwegian
-    "nno": "nn", // Norwegian Nynorsk
-    "nob": "nb", // Norwegian Bokmål
-    "swe": "sv", // Swedish
-    "fao": "fo", // Faroese
-
-    // Sami languages
-    "sme": "se", // Northern Sami
-
-    // Finno-Ugric languages
-    "fin": "fi", // Finnish
-    "est": "et", // Estonian
-    "hun": "hu", // Hungarian
-
-    // Celtic languages
-    "gle": "ga", // Irish
-    "gla": "gd", // Scottish Gaelic
-    "cym": "cy", // Welsh
-    "bre": "br", // Breton
-    "cor": "kw", // Cornish
-    "glv": "gv", // Manx
-
-    // Romance languages
-    "spa": "es", // Spanish
-    "fra": "fr", // French
-    "ita": "it", // Italian
-    "por": "pt", // Portuguese
-    "ron": "ro", // Romanian
-    "cat": "ca", // Catalan
-
-    // Slavic languages
-    "rus": "ru", // Russian
-    "pol": "pl", // Polish
-    "ces": "cs", // Czech
-    "slk": "sk", // Slovak
-    "ukr": "uk", // Ukrainian
-    "bel": "be", // Belarusian
-    "bul": "bg", // Bulgarian
-    "hrv": "hr", // Croatian
-    "srp": "sr", // Serbian
-    "slv": "sl", // Slovenian
-    "bos": "bs", // Bosnian
-    "mkd": "mk", // Macedonian
-
-    // Baltic languages
-    "lit": "lt", // Lithuanian
-    "lav": "lv", // Latvian
-
-    // Other European languages
-    "ell": "el", // Greek
-    "alb": "sq", // Albanian
-    "eus": "eu", // Basque
-    "mlt": "mt", // Maltese
-
-    // Greenlandic
-    "kal": "kl", // Kalaallisut (Greenlandic)
-
-    // Asian languages
-    "zho": "zh", // Chinese
-    "jpn": "ja", // Japanese
-    "kor": "ko", // Korean
-    "tha": "th", // Thai
-    "vie": "vi", // Vietnamese
-    "hin": "hi", // Hindi
-    "urd": "ur", // Urdu
-    "ben": "bn", // Bengali
-    "tam": "ta", // Tamil
-    "tel": "te", // Telugu
-    "mar": "mr", // Marathi
-    "guj": "gu", // Gujarati
-    "kan": "kn", // Kannada
-    "mal": "ml", // Malayalam
-    "ori": "or", // Odia
-    "pan": "pa", // Punjabi
-    "asm": "as", // Assamese
-    "nep": "ne", // Nepali
-    "sin": "si", // Sinhala
-    "mya": "my", // Myanmar
-    "khm": "km", // Khmer
-    "lao": "lo", // Lao
-    "mon": "mn", // Mongolian
-    "tib": "bo", // Tibetan
-
-    // Middle Eastern languages
-    "ara": "ar", // Arabic
-    "heb": "he", // Hebrew
-    "fas": "fa", // Persian
-    "tur": "tr", // Turkish
-    "aze": "az", // Azerbaijani
-    "kaz": "kk", // Kazakh
-    "kir": "ky", // Kyrgyz
-    "uzb": "uz", // Uzbek
-    "tgk": "tg", // Tajik
-    "tuk": "tk", // Turkmen
-    "tat": "tt", // Tatar
-    "bak": "ba", // Bashkir
-    "chv": "cv", // Chuvash
-    "sah": "sa", // Yakut
-
-    // African languages
-    "swa": "sw", // Swahili
-    "hau": "ha", // Hausa
-    "yor": "yo", // Yoruba
-    "ibo": "ig", // Igbo
-    "amh": "am", // Amharic
-    "som": "so", // Somali
-    "orm": "om", // Oromo
-    "tir": "ti", // Tigrinya
-    "kin": "rw", // Kinyarwanda
-    "run": "rn", // Kirundi
-    "mlg": "mg", // Malagasy
-    "sna": "sn", // Shona
-    "nde": "nd", // North Ndebele
-    "nbl": "nr", // South Ndebele
-    "sot": "st", // Southern Sotho
-    "tsn": "tn", // Tswana
-    "ven": "ve", // Venda
-    "xho": "xh", // Xhosa
-    "zul": "zu", // Zulu
-
-    // American languages
-    "que": "qu", // Quechua
-    "grn": "gn", // Guarani
-    "aym": "ay", // Aymara
-
-    // Pacific languages
-    "mao": "mi", // Māori
-    "fij": "fj", // Fijian
-    "ton": "to", // Tongan
-    "haw": "hh", // Hawaiian (non-standard, but sometimes used)
-
-    // Constructed languages
-    "epo": "eo", // Esperanto
-
-    // Additional languages that might be relevant
-    "ido": "io", // Ido
-    "ina": "ia", // Interlingua
-    "vol": "vo", // Volapük
+    aar: "aa",
+    abk: "ab",
+    afr: "af",
+    aka: "ak",
+    amh: "am",
+    ara: "ar",
+    arg: "an",
+    asm: "as",
+    ava: "av",
+    ave: "ae",
+    aym: "ay",
+    aze: "az",
+    bak: "ba",
+    bam: "bm",
+    bel: "be",
+    ben: "bn",
+    bis: "bi",
+    bod: "bo",
+    bos: "bs",
+    bre: "br",
+    bul: "bg",
+    cat: "ca",
+    ces: "cs",
+    cha: "ch",
+    che: "ce",
+    chu: "cu",
+    chv: "cv",
+    cor: "kw",
+    cos: "co",
+    cre: "cr",
+    cym: "cy",
+    dan: "da",
+    deu: "de",
+    div: "dv",
+    dzo: "dz",
+    ell: "el",
+    eng: "en",
+    epo: "eo",
+    est: "et",
+    eus: "eu",
+    ewe: "ee",
+    fao: "fo",
+    fas: "fa",
+    fij: "fj",
+    fin: "fi",
+    fra: "fr",
+    fry: "fy",
+    ful: "ff",
+    gla: "gd",
+    gle: "ga",
+    glg: "gl",
+    glv: "gv",
+    grn: "gn",
+    guj: "gu",
+    hat: "ht",
+    hau: "ha",
+    hbs: "sh",
+    heb: "he",
+    her: "hz",
+    hin: "hi",
+    hmo: "ho",
+    hrv: "hr",
+    hun: "hu",
+    hye: "hy",
+    ibo: "ig",
+    ido: "io",
+    iii: "ii",
+    iku: "iu",
+    ile: "ie",
+    ina: "ia",
+    ind: "id",
+    ipk: "ik",
+    isl: "is",
+    ita: "it",
+    jav: "jv",
+    jpn: "ja",
+    kal: "kl",
+    kan: "kn",
+    kas: "ks",
+    kat: "ka",
+    kau: "kr",
+    kaz: "kk",
+    khm: "km",
+    kik: "ki",
+    kin: "rw",
+    kir: "ky",
+    kom: "kv",
+    kon: "kg",
+    kor: "ko",
+    kua: "kj",
+    kur: "ku",
+    lao: "lo",
+    lat: "la",
+    lav: "lv",
+    lim: "li",
+    lin: "ln",
+    lit: "lt",
+    ltz: "lb",
+    lub: "lu",
+    lug: "lg",
+    mah: "mh",
+    mal: "ml",
+    mar: "mr",
+    mkd: "mk",
+    mlg: "mg",
+    mlt: "mt",
+    mon: "mn",
+    mri: "mi",
+    msa: "ms",
+    mya: "my",
+    nau: "na",
+    nav: "nv",
+    nbl: "nr",
+    nde: "nd",
+    ndo: "ng",
+    nep: "ne",
+    nld: "nl",
+    nno: "nn",
+    nob: "nb",
+    nor: "no",
+    nya: "ny",
+    oci: "oc",
+    oji: "oj",
+    ori: "or",
+    orm: "om",
+    oss: "os",
+    pan: "pa",
+    pli: "pi",
+    pol: "pl",
+    por: "pt",
+    pus: "ps",
+    que: "qu",
+    roh: "rm",
+    ron: "ro",
+    run: "rn",
+    rus: "ru",
+    sag: "sg",
+    san: "sa",
+    sin: "si",
+    slk: "sk",
+    slv: "sl",
+    sme: "se",
+    smo: "sm",
+    sna: "sn",
+    snd: "sd",
+    som: "so",
+    sot: "st",
+    spa: "es",
+    sqi: "sq",
+    srd: "sc",
+    srp: "sr",
+    ssw: "ss",
+    sun: "su",
+    swa: "sw",
+    swe: "sv",
+    tah: "ty",
+    tam: "ta",
+    tat: "tt",
+    tel: "te",
+    tgk: "tg",
+    tgl: "tl",
+    tha: "th",
+    tir: "ti",
+    ton: "to",
+    tsn: "tn",
+    tso: "ts",
+    tuk: "tk",
+    tur: "tr",
+    twi: "tw",
+    uig: "ug",
+    ukr: "uk",
+    urd: "ur",
+    uzb: "uz",
+    ven: "ve",
+    vie: "vi",
+    vol: "vo",
+    wln: "wa",
+    wol: "wo",
+    xho: "xh",
+    yid: "yi",
+    yor: "yo",
+    zha: "za",
+    zho: "zh",
+    zul: "zu"
   }
 
   // Check if we have a mapping for this 3-letter code
