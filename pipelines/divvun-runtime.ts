ba3d70c84d8825459e58a087ed269e5b03380001
Brendan Molloy <brendan@necessary.nu>
2025-10-30 00:08:03 +0100

    Update builder for divvun-runtime

--- pipelines/divvun-runtime.ts
+++ pipelines/divvun-runtime.ts
@@ -94,7 +94,7 @@ export async function pipelineDivvunRuntime() {
         label: `${target} - Build`,
         key: `cli-build-${target}`,
         command: [
-          `just build ${target}`,
+          `./x build ${target}`,
           `mv ${targetFile} ./divvun-runtime-unsigned-${target}`,
           `buildkite-agent artifact upload divvun-runtime-unsigned-${target}`,
         ],
@@ -124,7 +124,7 @@ export async function pipelineDivvunRuntime() {
       buildSteps.push(command({
         label: `${target}`,
         command: [
-          `just build ${target}`,
+          `./x build ${target}`,
           `mv ${targetFile} ./${artifactName}-${target} && buildkite-agent artifact upload ${artifactName}-${target}`,
         ],
         agents: {
@@ -147,7 +147,7 @@ export async function pipelineDivvunRuntime() {
       key: "playground-build",
       command: [
         "echo '--- Building UI'",
-        "just build-ui",
+        "./x build-ui",
         "cp -r './playground/src-tauri/target/release/bundle/macos/Divvun Runtime Playground.app' .",
         "echo '--- Updating version'",
         `/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${version}" './Divvun Runtime Playground.app/Contents/Info.plist'`,
