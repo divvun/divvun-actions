88a87afe5a1109abe8a6ad98df12c414e2702b6c
Brendan Molloy <brendan@necessary.nu>
2025-11-20 19:18:26 +0100

    I will cry

--- pipelines/static-lib-build/build-pytorch-linux.ts
+++ pipelines/static-lib-build/build-pytorch-linux.ts
@@ -200,6 +200,9 @@ export async function buildPytorchLinux(options: BuildPytorchLinuxOptions) {
   // Set C++17 standard explicitly
   cmakeArgs.push("-DCMAKE_CXX_STANDARD=17")
 
+  // Enable position independent code for static libraries
+  cmakeArgs.push("-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
+
   // Static or shared libraries
   if (shared) {
     cmakeArgs.push("-DBUILD_SHARED_LIBS=ON")
